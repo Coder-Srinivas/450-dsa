fe271d72a95651534b78d5c7ee6a91c8ac346c4f
Coder-Srinivas <srinivas.babu364@gmail.com>
2021-10-04 19:38:03 +0530

    Added Reverse Array

---
+++ Arrays/ReverseArray.java
@@ -0,0 +1,10 @@
+static void rvereseArray(int arr[], int start, int end){
+    int temp;
+    while (start < end){
+    temp = arr[start];
+    arr[start] = arr[end];
+    arr[end] = temp;
+    start++;
+    end--;
+    }
+} 
